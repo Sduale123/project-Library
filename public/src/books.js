fbc5b69a77536158b8391c2481c0bb32a6876d64
Saciid <123839960+Sduale123@users.noreply.github.com>
2024-01-27 12:15:13 -0600

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,14 +1,18 @@
+// Function to find an author by their ID in a list of authors
 function findAuthorById(authors, id) {
+  // Using the find() method to locate the author with the specified ID
   let authorById = authors.find((author) => author.id === id);
-  return authorById;
+  return authorById; // Returning the author object found
 }
 
+// Function to find a book by its ID in a list of books
 function findBookById(books, id) {
+  // Using the find() method to locate the book with the specified ID
   let bookById = books.find((book) => book.id === id);
-  return bookById;
+  return bookById; // Returning the book object found
 }
 
-// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+// Function to partition books based on their borrowed status
 function partitionBooksByBorrowedStatus(books) {
   return books.reduce(
     (acc, book) => {
@@ -26,6 +30,7 @@ function partitionBooksByBorrowedStatus(books) {
   );
 }
 
+// Function to get a list of borrowers for a given book
 function getBorrowersForBook(book, accounts) {
   const accountsById = accounts.reduce((acc, account) => {
     acc[account.id] = account;
@@ -37,7 +42,7 @@ function getBorrowersForBook(book, accounts) {
       ...accountsById[id],
       returned,
     }))
-    .slice(0, 10);
+    .slice(0, 10); // Returning only the first 10 borrowers
 }
 
 module.exports = {
